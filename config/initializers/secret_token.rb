214fb62c81e500231c599e3baf176ac12aca0bfd
chaos <yume_@live.com>
2013-02-12 22:24:54 +0300

    spree, ru, fancy. no customz

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spree::Application.config.secret_token = '149fd27960f96973c935b2d937f4b08d9b25a1c6be1fc018f3164a192035a38e48a0e33efbf7faf4fc46e49979aae35871802a4f44a1fe851deae79c064426f9'
